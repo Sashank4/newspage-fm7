d90a613145c9902df8f015d3f896da93d3d853d7
sashank <sashanlt4@gmail.com>
2024-04-19 22:33:29 +0530

    commit with animation

--- src/App.js
+++ src/App.js
@@ -5,20 +5,46 @@ import mainImgMobile from './assets/images/image-web-3-mobile.jpg'
 import sencondImg1 from './assets/images/image-retro-pcs.jpg'
 import sencondImg2 from './assets/images/image-top-laptops.jpg'
 import sencondImg3 from './assets/images/image-gaming-growth.jpg'
-
+import {motion} from "framer-motion";
 function App() {
   return (
     <div className="App p-6 md:p-24"> 
       <Navbar/>
 
       <div className='pt-6 flex-column md:grid md:grid-cols-3 md:grid-rows-2 gap-8 grid-flow-row-dense grid-auto-rows'>
-        <div className='col-span-2 hidden sm:block'>
+        <motion.div className='col-span-2 hidden sm:block' 
+          initial = {{
+            opacity:0,
+            y:-50 
+          }}
+          whileInView={{
+            opacity:1,
+            y:0,
+            transition:{
+              duration:1
+            }
+          }}
+          viewport={{ once: true }}
+          >
           <img src={mainImg} alt='mainImg'/>
-        </div>
+        </motion.div>
         <div className='md:hidden'>
           <img src={mainImgMobile} alt='mainImgMobile'/>
         </div>
-        <div className='md:mt-0 mt-4 latest-news row-span-2 h-5/6'>
+        <motion.div className='md:mt-0 mt-4 latest-news row-span-2 h-5/6'
+        initial = {{
+          opacity:0,
+          x:50 
+        }}
+        whileInView={{
+          opacity:1,
+          x:0,
+          transition:{
+            duration:1
+          }
+        }}
+        viewport={{ once: true }}
+          >
           <div className='p-10'>
               <p className=' heading text-4xl pb-4 font-bold text-start text-white'>New</p>
               <div className='pt-4'>
@@ -36,20 +62,59 @@ function App() {
               <p className='text-start pt-3 subTag text-white text-lg font-light'>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
               </div>
           </div>
-        </div>
+        </motion.div>
 
-        <div>
+        <motion.div
+        initial = {{
+          opacity:0,
+          x:-50 
+        }}
+        whileInView={{
+          opacity:1,
+          x:0,
+          transition:{
+            duration:1
+          }
+        }}
+        viewport={{ once: true }}
+         >
           <p className='text-7xl bright-heading font-bold text-start'>The Bright Future of Web 3.0?</p>
-        </div>
+        </motion.div>
 
-        <div className='fourth-column p-4 '>
+        <motion.div className='fourth-column p-4 '
+        initial = {{
+          opacity:0,
+          y:50 
+        }}
+        whileInView={{
+          opacity:1,
+          y:0,
+          transition:{
+            duration:1
+          }
+        }}
+        viewport={{ once: true }}
+         >
           <p className='text-start text-lg pb-12'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
           But is it really fulfilling its promise?</p>
           <button className='read-more-button pl-6 pr-6 float-left'><p className='p-2 tracking-wider font-semibold text-white'>Read More</p></button>
-        </div>
+        </motion.div>
       </div>
 
-      <div className='pt-6 flex-column md:grid md:grid-cols-3 md:grid-rows-2 md:gap-8 thirdSection'>
+      <motion.div className='pt-6 flex-column md:grid md:grid-cols-3 md:grid-rows-2 md:gap-8 thirdSection'
+      initial = {{
+        opacity:0,
+        y:50 
+      }}
+      whileInView={{
+        opacity:1,
+        y:0,
+        transition:{
+          duration:1
+        }
+      }}
+      viewport={{ once: true }}
+      >
         <div className='flex pt-4 pb-4 md:p-2'>
            <div> <img src={sencondImg1} alt='secondImg1' /> </div>
             <div className='pl-4 pr-4 flex-column' >
@@ -74,7 +139,7 @@ function App() {
               <p className='text-start leading-normal data'>How the pandemic has sparked fresh opportunities.</p>
             </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
